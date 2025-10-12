e19c12615bafe444bb59654c19961b7a3e51fe91
brodaclop <biziclop@gmail.com>
2025-10-12 17:02:29 +0100

    dice roller

--- client/interpolation.ts
+++ client/interpolation.ts
@@ -1,5 +1,6 @@
 
 const DICE_PATTERN = /\b(\d+)D\b/g;
+const CLASSIC_DICE_PATTERN = /(?<count>\d+)?d(?<dice>\d+)(?<mod>[+-]\d+)?/g;
 
 const rollSuccessDice = (num: number): number => {
     let total: number = 0;
@@ -15,10 +16,14 @@ const rollSuccessDice = (num: number): number => {
     return total;
 }
 
+const rollNormalDice = (count: string | undefined, dice: string, mod?: string) =>
+    Array(Number(count ?? 1)).fill(undefined).reduce(((acc, curr) => acc + (Math.floor(Math.random() * Number(dice)) + 1)), Number(mod ?? 0));
+
 export const Interpolation = {
 
     perform: (message: string): string => {
-        const output = message.replaceAll(DICE_PATTERN, (match, num) => `${match} = ${rollSuccessDice(num)}`);
+        let output = message.replaceAll(DICE_PATTERN, (match, num) => `${match} = ${rollSuccessDice(num)}`);
+        output = output.replaceAll(CLASSIC_DICE_PATTERN, (match, count, dice, mod) => `${match} = ${rollNormalDice(count, dice, mod)}`);
         return output;
     }
 }
